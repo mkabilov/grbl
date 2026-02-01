6642508b3bea27f56f0f5fc3277e4202349ae2e4
Murat Kabilov <murat@kabilov.com>
2026-02-01 11:38:55 +0100

    * adjust for LY Drawbot * fix warning in eeprom.c

--- grbl/eeprom.c
+++ grbl/eeprom.c
@@ -124,13 +124,11 @@ void eeprom_put_char( unsigned int addr, unsigned char new_value )
 	sei(); // Restore interrupt flag state.
 }
 
-// Extensions added as part of Grbl 
-
-
+// Extensions added as part of Grbl
 void memcpy_to_eeprom_with_checksum(unsigned int destination, char *source, unsigned int size) {
   unsigned char checksum = 0;
   for(; size > 0; size--) { 
-    checksum = (checksum << 1) || (checksum >> 7);
+    checksum = (unsigned char)(checksum << 1 | checksum >> 7);
     checksum += *source;
     eeprom_put_char(destination++, *(source++)); 
   }
@@ -141,7 +139,7 @@ int memcpy_from_eeprom_with_checksum(char *destination, unsigned int source, uns
   unsigned char data, checksum = 0;
   for(; size > 0; size--) { 
     data = eeprom_get_char(source++);
-    checksum = (checksum << 1) || (checksum >> 7);
+    checksum = (unsigned char)(checksum << 1 | checksum >> 7);
     checksum += data;    
     *(destination++) = data; 
   }
